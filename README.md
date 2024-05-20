8ce76ec4e5d3a951ed4a7ae63cc91254a9ff76fb
odmoreno <oscarpol95@gmail.com>
2024-05-20 09:08:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Carrito de compras de guitarras probando hoooks
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  const name = "Oscar"
+  return (
+    <>
+      <h1>Hola : {name}</h1>
+      <p> Return</p>
+    </>
+  )
+}
